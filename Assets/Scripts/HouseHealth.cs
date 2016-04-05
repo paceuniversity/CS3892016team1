07c7188d9b662366294a2f1c556b09aa3ee3dd96
rgee258 <rgee258@gmail.com>
2016-04-04 21:59:58 -0400

    Second commit with Unity based files and simple preface

---
+++ Assets/Scripts/HouseHealth.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HouseHealth : MonoBehaviour {
+
+	public int health = 1;
+
+	// Do we need this? Consideration in case someone taps on the house
+	public bool isEnemy = false;
+
+	public void takeDamage(int damageVal)
+	{
+		health -= damageVal;
+
+		if (health <= 0) {
+			//Destroys the gameObject the script is attached to, in this case the house
+			Destroy (gameObject);
+		}
+	}
+
+	void Update() {
+
+	}
+}
