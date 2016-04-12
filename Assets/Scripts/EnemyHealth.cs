5be1ba5b10dba0c5c4f9ded866c2cb13617e721e
nida1994 <nida.b94@gmail.com>
2016-04-12 14:42:44 -0400

    updated info page

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+	public int health = 1;
+	public int damage = 1;
+
+	public void takeDamage(int damageVal)
+	{
+		health -= damageVal;
+
+		if (health <= 0) {
+			//Destroys the gameObject the script is attached to, in this case the mosquito
+			Destroy (gameObject);
+		}
+	}
+
+	void Update() {
+
+	}
+}
