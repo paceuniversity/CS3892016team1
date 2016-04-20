3ef3552546e75a65c718df6a09ec10e727e918e0
nida1994 <nida.b94@gmail.com>
2016-04-19 22:16:54 -0400

    Revised Code

---
+++ UpdatedCS3892016team1-master /Assets/Scripts/House.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class House : MonoBehaviour {
+
+	private int health = 10;
+
+	public void takeDamage(int damageVal)
+	{
+		health -= damageVal;
+
+		if (health <= 0) {
+			// Destroys the gameObject the script is attached to, in this case the house
+			Destroy (gameObject);
+            Application.LoadLevel("GameOver");
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if (col.gameObject.tag == "Enemy")
+		{
+			takeDamage (1);
+			Destroy (col.gameObject);
+		}
+	}
+}
