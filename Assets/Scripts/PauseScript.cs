b78c01dc652d22b8b06e5e00ff0e2c184fa2884f
pedrobpio <pedrobpio@gmail.com>
2016-04-21 02:10:27 -0400

    added a pause menu, it will be changed latter

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class PauseScript : MonoBehaviour {
     public bool paused;
+    public GameObject menu;
 	// Use this for initialization
 	void Start () {
         paused = false;
-	}
+        Time.timeScale = 1;
+        menu.SetActive(paused);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,10 +24,13 @@ public class PauseScript : MonoBehaviour {
         if (paused)
         {
             Time.timeScale = 0;
+            menu.SetActive(paused);
+
         }
         else if (!paused)
         {
             Time.timeScale = 1;
+            menu.SetActive(paused);
         }
     }
 }
