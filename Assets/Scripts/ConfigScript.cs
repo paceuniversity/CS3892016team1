ef674c0cb1df01e0bc3b0d18424e7a9d27b32e98
pedrobpio <pedrobpio@gmail.com>
2016-04-22 15:04:07 -0400

    changed design

---
+++ Assets/Scripts/ConfigScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ConfigScript : MonoBehaviour {
+
+    public Text info;
+    public GameObject settings;
+    public Text title;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	public void Settings()
+    {
+        info.gameObject.SetActive(false);
+        settings.SetActive(true);
+    }
+    public void changeText(Text choosen)
+    {
+        settings.SetActive(false);
+        info.gameObject.SetActive(true);
+        info.text = choosen.text;
+    }
+    public void changeTitle(string newTitle)
+    {
+        title.text = newTitle;
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
